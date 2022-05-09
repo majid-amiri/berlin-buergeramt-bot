817a1be2605fb32c272ff2acd4bfc99b752b3ce0
Majid Amiri <majidamiriii@gmail.com>
2022-05-09 15:29:18 +0200

    fix timezone

--- getAvailableAppointments.js
+++ getAvailableAppointments.js
@@ -7,8 +7,12 @@ function getAvailableAppointments(html) {
     .map((_, appointmentLink) => {
       const appointmentUrl = appointmentLink.attribs["href"];
       let appointmentDate = appointmentUrl.split("/")[4];
-      appointmentDate = new Date(appointmentDate * 1000);
-      appointmentDate = Intl.DateTimeFormat("de-DE").format(appointmentDate);
+      appointmentDate = new Date(appointmentDate * 1000).toLocaleDateString(
+        "de-DE",
+        {
+          timeZone: "Europe/Berlin",
+        }
+      );
 
       return { date: appointmentDate, url: process.env.BOOKING_URL };
     })
