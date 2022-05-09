f38cfb38a5373f059bcbcba177905744d18b4648
Majid Amiri <majidamiriii@gmail.com>
2022-05-09 02:12:07 +0200

    change booking url

--- getAvailableAppointments.js
+++ getAvailableAppointments.js
@@ -5,15 +5,12 @@ function getAvailableAppointments(html) {
   const appointmentLinks = $("td.buchbar a");
   return appointmentLinks
     .map((_, appointmentLink) => {
-      // url
       const appointmentUrl = appointmentLink.attribs["href"];
-      const hostUrl = new URL(process.env.BOOKING_URL).origin;
-
-      // date
       let appointmentDate = appointmentUrl.split("/")[4];
       appointmentDate = new Date(appointmentDate * 1000);
       appointmentDate = Intl.DateTimeFormat("de-DE").format(appointmentDate);
-      return { date: appointmentDate, url: hostUrl + appointmentUrl };
+
+      return { date: appointmentDate, url: process.env.BOOKING_URL };
     })
     .toArray();
 }
